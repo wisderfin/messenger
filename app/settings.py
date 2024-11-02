3144c2836670286847911ecbeec3bf2de4250703
wisderfin <kassas20@mail.ru>
2024-11-02 12:39:42 +0400

    add volumes in app-container & rename app-container to api-container

--- app/settings.py
+++ app/settings.py
@@ -1,5 +1,5 @@
 from typing import List
-from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     CORS_ORIGINS: List[str]
